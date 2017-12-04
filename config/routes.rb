21006429d5107c831c2c197319fb7e52ed9cfd00
La Van Bac <la.van.bac@framgia.com>
2017-12-04 15:05:34 +0700

    Basic login

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
 
   get "static_pages/home"
   get "static_pages/help"
-  get "sign_up", to: "users#new"
+
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  get "/signup", to: "users#new"
 
   resources :users
 end
