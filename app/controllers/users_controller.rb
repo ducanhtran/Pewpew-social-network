1590c7d9cb7cb51edf9c7e62143cc32654ad3247
La Van Bac <la.van.bac@framgia.com>
2017-12-05 17:10:19 +0700

    Advanced login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,8 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find params[:id]
+    @user = User.find_by id: params[:id]
+    redirect_to root_path unless @user
   end
 
   def new
